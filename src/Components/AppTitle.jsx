fd6a647cddcbb370713efe4065c8844feb1d512c
ByteCodeSculptorPro <byte.code.sculptor@gmail.com>
2024-10-18 14:31:26 +0200

    added text box for searching, serach for shows on button click

--- src/Components/AppTitle.jsx
+++ src/Components/AppTitle.jsx
@@ -1,6 +1,6 @@
 export default function AppTitle({
   title = 'Box-office',
-  subtitle = 'Are you looking for a movie or an app ?',
+  subtitle = 'Are you looking for a movie or an actor ?',
 }) {
   return (
     <div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,31 @@
+import { useState } from 'react';
+
 export default function Home() {
-  return <div>Home</div>;
+  // eslint-disable-next-line no-unused-vars
+  const [searchStr, setSearchStr] = useState('');
+
+  const onSearchInputChange = ev => {
+    setSearchStr(ev.target.value);
+  };
+
+  const onSearch = async ev => {
+    ev.preventDefault();
+
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchStr}`
+    );
+    const body = await response.json();
+
+    console.log(body);
+
+    // https://api.tvmaze.com/search/shows?q=girls
+  };
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" onChange={onSearchInputChange} />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 }
