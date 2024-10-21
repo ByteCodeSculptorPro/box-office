62635c04034651bbad360de23d336a3340d12325
ByteCodeSculptorPro <byte.code.sculptor@gmail.com>
2024-10-21 21:06:35 +0200

    extract data fetching logic in a custom hook useGetShowById on top up built in hooks

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,24 +1,27 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
-
-export default function Show() {
-  const { showId } = useParams();
+const useGetShowById = showId => {
   const [showData, setShowData] = useState(null);
   const [showError, setShowError] = useState(null);
-
   useEffect(() => {
     async function fetchData() {
       try {
         const data = await getShowById(showId);
         setShowData(data);
-        console.log(showData);
       } catch (error) {
         setShowError(error);
       }
     }
     fetchData();
   }, [showId]);
+
+  return { showData, showError };
+};
+
+export default function Show() {
+  const { showId } = useParams();
+  const { showData, showError } = useGetShowById(showId);
 
   if (showError) {
     return <div>We have an error:{showError.message}</div>;
