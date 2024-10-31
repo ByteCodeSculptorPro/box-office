b62637d6824a6a661c8d7a790077f45308b4472e
ByteCodeSculptorPro <byte.code.sculptor@gmail.com>
2024-10-31 14:42:06 +0100

    added initial logic to star shows with with use reducer. created use pesristedreducer to syn state with localStorage

--- src/Components/shows/ShowGrid.jsx
+++ src/Components/shows/ShowGrid.jsx
@@ -1,6 +1,40 @@
+import { useEffect, useReducer } from 'react';
 import ShowCard from './ShowCard';
 
+const usePresistedReducer = (reducer, initialState, localStorageKey) => {
+  const [state, dispatch] = useReducer(reducer, initialState, initial => {
+    const persistedValue = localStorage.getItem(localStorageKey);
+
+    return persistedValue ? JSON.parse(persistedValue) : initial;
+  });
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(state));
+  }, [state, localStorageKey]);
+  return [state, dispatch];
+};
+const starredShowsReducer = (currentStarred, action) => {
+  switch (action.type) {
+    case 'STAR':
+      return [...currentStarred, action.showId];
+    case 'UNSTAR':
+      return currentStarred.filter(showId => showId != action.showId);
+    default:
+      return currentStarred;
+  }
+};
 export default function ShowGrid({ shows }) {
+  // const [starredShows, dispatchStarred] = useReducer(starredShowsReducer, []);
+  const [starredShows, dispatch] = usePresistedReducer(
+    starredShowsReducer,
+    [],
+    'starredShows'
+  );
+  const onStarMeClick = showId => {
+    const isStarred = starredShows.includes(showId);
+    if (isStarred) dispatch({ type: 'UNSTAR', showId });
+    else dispatch({ type: 'STAR', showId });
+  };
+  console.log({ starredShows });
   return (
     <div className="show-grid">
       {shows.map(data => (
@@ -12,6 +46,7 @@ export default function ShowGrid({ shows }) {
           image={
             data.show.image ? data.show.image.medium : '/not-found-image.png'
           }
+          onStarMeClick={onStarMeClick}
         />
       ))}
     </div>
