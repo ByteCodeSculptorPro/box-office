2f38e733409335d274c40d6e5343fcb9f0ebe7da
ByteCodeSculptorPro <byte.code.sculptor@gmail.com>
2024-11-01 14:36:03 +0100

    fetch starred shows data and display ShowGrid for them

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,7 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
 import { useStarredShows } from '../lib/useStarredShows';
+import { getShowsByIds } from '../api/tvmaze';
+import ShowGrid from '../Components/shows/ShowGrid';
 
 export default function Starred() {
-  const [starredShows] = useStarredShows();
+  const [starredShowsIds] = useStarredShows();
 
-  return <div>Starred page, starred {starredShows.length}</div>;
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['starred', starredShowsIds],
+    queryFn: () =>
+      getShowsByIds(starredShowsIds).then(result =>
+        result.map(show => ({
+          show,
+        }))
+      ),
+    refetchOnWindowFocus: false,
+
+    // {
+    // const apiRequestPromises = starredShows.map(showId =>
+    //   getShowsByIds(showId)
+    // );
+    // await Promise.all(apiRequestPromises);
+    // },
+  });
+
+  if (starredShows?.length == 0) return <div>No shows were starred</div>;
+  if (starredShows?.length > 0)
+    return <ShowGrid shows={starredShows}></ShowGrid>;
+  if (starredShowsError)
+    return <div>An error occured: {starredShowsError.message}</div>;
+
+  return <div>Starred shows loading...</div>;
 }
